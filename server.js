64c198c298f7ed29bcb6ff69a93ee3ca688ce409
stevennedwinn <108059776+stevennedwinn@users.noreply.github.com>
2022-07-15 18:00:25 +0800

    Gmail protocol updates, user and pass updates

--- server.js
+++ server.js
@@ -3,41 +3,45 @@ var express = require('express'),
     nodeMailer = require('nodemailer'),
     bodyParser = require('body-parser');
 
-    var app = express();
-    app.set('view engine', 'ejs');
-    app.use(express.static('public'));
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    var port = 3000;
-    app.get('/', function (req, res) {
-      res.render('index');
-    });
-    app.post('/send-email', function (req, res) {
-      let transporter = nodeMailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          auth: {
-              user: 'xxx@xx.com',
-              pass: 'xxxx'
-          }
-      });
-      let mailOptions = {
-          from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
-          to: req.body.to, // list of receivers
-          subject: req.body.subject, // Subject line
-          text: req.body.body, // plain text body
-          html: '<b>NodeJS Email Tutorial</b>' // html body
-      };
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = 3000;
 
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
-              res.render('index');
-          });
-      });
-          app.listen(port, function(){
-            console.log('Server is running at port: ',port);
-          });
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+app.post('/send-email', function (req, res) {
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'vft.iot@gmail.com',
+            pass: 'oadayzueyqujtwif'
+        }
+    });
+    let mailOptions = {
+        // from: '"Steven Handsome" <ssseew17@gmail.com>', // sender address
+        from: '"VFlowtech IoT Alert"', // sender address
+        to: req.body.to, // list of receivers
+        subject: req.body.subject, // Subject line
+        text: req.body.body, // plain text body // template alert message to be inserted
+        // html: '<b>NodeJS Email Tutorial</b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+        res.render('index');
+    });
+});
+app.listen(port, function () {
+    console.log('Server is running at port: ', port);
+});
