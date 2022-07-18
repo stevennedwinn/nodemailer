a408f8e9b3d3a58e85c3fe6e2ef1882214c3ac27
stevennedwinn <108059776+stevennedwinn@users.noreply.github.com>
2022-07-18 17:19:04 +0800

    function changes

--- server.js
+++ server.js
@@ -12,10 +12,41 @@ var port = 3000;
 
 app.get('/', function (req, res) {
     res.render('index');
+    res
 });
 
+// app.post('/send-email', function (req, res) {
+//     // console.log("////////////////////////////////////////////////////////////////////////////")
+//     // console.log("req: ", req)
+//     // console.log("res: ", res)
+//     let transporter = nodeMailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: 'vft.iot@gmail.com',
+//             pass: 'oadayzueyqujtwif'
+//         }
+//     });
+//     let mailOptions = {
+//         // from: '"Steven Handsome" <ssseew17@gmail.com>', // sender address
+//         from: '"VFlowtech IoT Alert"', // sender address
+//         to: req.body.to, // list of receivers
+//         subject: req.body.subject, // Subject line
+//         text: req.body.body, // plain text body // template alert message to be inserted
+//         // html: '<b>NodeJS Email Tutorial</b>' // html body
+//     };
 
-app.post('/send-email', function (req, res) {
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log('Message %s sent: %s', info.messageId, info.response);
+//         res.render('index');
+//     });
+// });
+
+function alertMsgTemplate (receiver, subject, text){
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -28,9 +59,9 @@ app.post('/send-email', function (req, res) {
     let mailOptions = {
         // from: '"Steven Handsome" <ssseew17@gmail.com>', // sender address
         from: '"VFlowtech IoT Alert"', // sender address
-        to: req.body.to, // list of receivers
-        subject: req.body.subject, // Subject line
-        text: req.body.body, // plain text body // template alert message to be inserted
+        to: receiver, // list of receivers
+        subject: subject, // Subject line
+        text: text, // plain text body // template alert message to be inserted
         // html: '<b>NodeJS Email Tutorial</b>' // html body
     };
 
@@ -41,7 +72,12 @@ app.post('/send-email', function (req, res) {
         console.log('Message %s sent: %s', info.messageId, info.response);
         res.render('index');
     });
-});
+
+    console.log(receiver, subject, text);
+}
+
+alertMsgTemplate('vft.iot.test@gmail.com', 'Test2', 'templateMsg'); // call the function
+
 app.listen(port, function () {
     console.log('Server is running at port: ', port);
 });
